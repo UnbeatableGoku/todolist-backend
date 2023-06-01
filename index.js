d03c4912cff56035df286a0796082c47435d6572
UnbeatableGoku <99610972+UnbeatableGoku@users.noreply.github.com>
2023-06-01 16:42:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cookieparser());
 // app.use(cors({ origin: 'https://mongodb-todolist-client.vercel.app', credentials: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://todolist-frontend-peach.vercel.app', credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
